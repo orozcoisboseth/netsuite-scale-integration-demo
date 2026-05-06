1582ba67c41eec93a9cf50d903110b9d20ceccf8
orozcoisboseth <orozco.isboseth@gmail.com>
2026-05-06 10:22:49 -0700

    Create test_scale_adapter.js

---
+++ tests/test_scale_adapter.js
@@ -0,0 +1,15 @@
+
+// Simple local test for the scale adapter
+const scaleAdapter = require('../src/adapters/scale_api_adapter_mock');
+
+function testGetWeight() {
+  const result = scaleAdapter.getWeight();
+
+  console.assert(result.success === true, 'Expected success');
+  console.assert(result.weight > 0, 'Weight should be > 0');
+  console.assert(result.unit === 'kg', 'Unit should be kg');
+
+  console.log(' Scale adapter mock test passed:', result);
+}
+
+testGetWeight();
